54dba5e7ee9d55ecb2f9406eacae0cbcd31b34c4
Martin Goldman <mgoldman@skytap.com>
2015-08-05 07:12:42 -0400

    doc list sort of works

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -6,6 +6,6 @@ module DocsHelper
   end    
 
   def excerpt_from(text, link_url)
-    truncate(strip_tags(text), length: 250)
+    truncate(strip_tags(text), length: 250, escape: false, separator: ' ')
   end
 end
